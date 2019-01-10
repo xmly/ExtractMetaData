af880e7537fc88d24d74ddbc93e012452544d198
Phanikumar Adiraju <phani.adiraju1403@gmail.com>
2019-01-09 22:26:53 -0500

    [REMOVE] Removed unwanted code

--- index.js
+++ index.js
@@ -1,77 +1,11 @@
 const path = require('path');
-const { EOL } = require('os');
-const { Transform } = require('stream');
 const readdirp = require('readdirp');
 var requiredFunctions = require('./requiredFunctions')
 var utils = require('./utils')
 
-/*
- * Print out all JavaScript files within the current folder and
- * subfolders along with their size.
- */
-
-let dirname = '../../../../GNSTemple/code/gns-temp'
+const dirname = '../../../../GNSTemple/code/gns-temp'
 const dirname2 = '../TestProjects/javatest-maven'
 const dirname3 = '../TestProjects/javatest-gradle'
-
-var extensions = []
-var runTimes = []
-var unknownRunTimes = []
-
-function extractRunTimes() {
-  const entryInfoStream = readdirp({
-    root: path.join(dirname),
-    fileFilter: '*.*',
-    directoryFilter: [ '!.git', '!*modules' ],
-  });
-
-  entryInfoStream
-    .on('warn', (err) => {
-      console.error('non-fatal error', err);
-      // Optionally call stream.destroy() here in order to abort and cause 'close' to be emitted
-    })
-    .on('error', err => console.error('fatal error', err))
-    .on('end', () => {
-      extensions.forEach((extension) => {
-        let runTime = utils.extensionMapper.find(obj => {
-          // console.log('ext findTech: ', '.'+extension.toUpperCase());
-         return obj.fileExtension === '.'+extension.toUpperCase()
-       })
-       if(typeof(runTime)!=='undefined' && !runTimes.includes(runTime)){
-         runTimes.push(runTime.technology)
-       }
-       if(typeof(runTime)==='undefined' && !runTimes.includes(runTime)){
-         unknownRunTimes.push(extension)
-       }
-      })
-      var result = 'hi'
-      return result
-    })
-    .pipe(new Transform({
-      objectMode: true,
-      transform(entryInfo, encoding, callback) {
-        // Turn each entry info into a more simplified representation
-        this.push({ path: entryInfo.path, size: entryInfo.stat.size });
-        callback();
-      },
-    }))
-    .pipe(new Transform({
-      objectMode: true,
-      transform(entryInfo, encoding, callback) {
-        // Turn each entry info into a string with a line break
-        this.push(`${JSON.stringify(entryInfo)}${EOL}`);
-        callback();
-      },
-    }))
-    .on('data', function (entry) {
-      let extension = requiredFunctions.findExtension(JSON.parse(entry).path)
-      if(!extensions.includes(extension)){
-        extensions.push(extension)
-      }
-      // -- need to check for gradle, maven, ant, webpack, nodemodules, git etc
-    })
-
-}
 
 function readFiles() {
   return new Promise((resolve,reject) => {
@@ -101,7 +35,6 @@ function readFiles() {
     });
   })
 }
-
 
 function main() {
   console.log('started main');
